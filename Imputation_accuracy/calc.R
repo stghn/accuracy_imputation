ce07b72c4b7da1ca377f7ddff67077d70ad091fd
Sajjad Toghiani <stoghiani@gmail.com>
2021-12-15 11:33:40 -0500

    Update calc.R

--- Imputation_accuracy/calc.R
+++ Imputation_accuracy/calc.R
@@ -2,17 +2,17 @@ calc.imp.accuracy <- function(truegeno,imputedgeno,mapHD,mapLD,nIID,format,accur
   ## Date : 24:09:2015
   options(warn=-1)
   #==================================================================================================#
-  # Truegeno == filename of true genotype (PLINK or allele dosage or gene content format)
-  # Imputedgeno == filename of true genotype (PLINK or allele dosage or gene contetn format)
-  # note that both imputedgeno and truegeno should have the same file format
+  # truegeno == filename of true genotype (PLINK or allele dosage or gene content format)
+  # imputedgeno == filename of true genotype (PLINK or allele dosage or gene content format)
   # mapHD == filename of the HIGH density SNP map file
   # mapLD == filename of the LOW density or imputed genotype SNP map file
   # nIID == number of animals genotyped, you can specify higher number but not less
   # format == The format of the genotype data (either PLINK or gene content/allele dosage format)
   # accuracy_type == The method to use in calculating accuracy (1. simple 2. calus_mulder 3. both)
-  # Read: Evaluation of measures of correctness of genotype imputation in the context of genomic prediction: 
-  # a review of livestock applications (Calus et al. 2014 - http://dx.doi.org/10.1017/S1751731114001803 )
-  # missgeno == the value of the missing genotype in the data file (eg. NA, 5, -9 ...)
+  # missgeno == the value of the missing genotype in the data file (eg. NA, 5, -9 ...) 
+  #### Note: Both imputedgeno and truegeno arguements should have the same file format 
+  #### Read: Evaluation of measures of correctness of genotype imputation in the context of genomic prediction: 
+  #### a review of livestock applications (Calus et al. 2014 - http://dx.doi.org/10.1017/S1751731114001803 )
   #==================================================================================================#
   
   if (missing(truegeno))
